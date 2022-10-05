25a75f2a870783ae5774b2d6187905c90a1c6471
sliacky <register4marketing@gmail.comgit congig --global user.email git config user.email register4marketing@gmail.com>
2022-10-05 14:44:59 +0200

    pridana myslienkova mapa

---
+++ chovatelske_stanice.mm
@@ -0,0 +1,425 @@
+<map version="freeplane 1.9.8">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Aplikacia SK chovatelskych stanic" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1664972602469" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="0.683">
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" associatedTemplateLocation="template:/standard-1.6.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      databaza vsetkych chovatelskych stanic na Slovensku
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Useful links:
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Domain knowledge:
+    </p>
+    <p>
+      https://github.com/batogov/cat-breeds-classifier
+    </p>
+    <p>
+      https://www.purina.com/cats/cat-breeds
+    </p>
+    <p>
+      https://www.petfinder.com/cat-breeds/
+    </p>
+    <p>
+      http://fifeweb.org/breeds
+    </p>
+    <p>
+      tica.org
+    </p>
+    <p>
+      cfa.org
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Funkcionalita:
+    </p>
+    <p>
+      topdoktor.sk
+    </p>
+    <p>
+      lekar.sk
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Musi bezat 14 dnova zaloha filesystem aj db
+    </p>
+  </body>
+</html>
+</richcontent>
+<richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      databaza s vyhladavanim
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="zadanie" POSITION="right" ID="ID_995253494" CREATED="1660909738463" MODIFIED="1660909742730">
+<edge COLOR="#0000ff"/>
+<node TEXT="cca. 1000 stanic" ID="ID_1806403348" CREATED="1660909746632" MODIFIED="1660909764437"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      macky
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="cca. 2500 stanic" ID="ID_729118217" CREATED="1660909767031" MODIFIED="1660909773806"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      psy
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="funkcionalita" POSITION="left" ID="ID_587557156" CREATED="1660909788016" MODIFIED="1660913301718">
+<edge COLOR="#00ff00"/>
+<node TEXT="zoznam stanic" ID="ID_181069497" CREATED="1660909792815" MODIFIED="1664967813723"><richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      link na formular, ktory vyplna stanica, ale pridava to administrator.
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="triedenie" ID="ID_179230824" CREATED="1660909887143" MODIFIED="1664967866264"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      filter
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="plemeno" ID="ID_55301497" CREATED="1660909890607" MODIFIED="1660909892287"/>
+<node TEXT="lokalita" ID="ID_1037570778" CREATED="1660909893560" MODIFIED="1660909895200"/>
+<node TEXT="tag" ID="ID_345863302" CREATED="1660909955125" MODIFIED="1660909956617"/>
+<node TEXT="nazov stanice" ID="ID_517762770" CREATED="1664967875913" MODIFIED="1664967880101"/>
+<node TEXT="rating" ID="ID_895270505" CREATED="1664967891488" MODIFIED="1664967897094"/>
+</node>
+<node TEXT="detail stanice" ID="ID_536959563" CREATED="1660909902079" MODIFIED="1660909904787">
+<node TEXT="popis max 500 slov" ID="ID_791498381" CREATED="1660909906006" MODIFIED="1660909912702"/>
+<node TEXT="fotografie max 5" ID="ID_755889650" CREATED="1660909914535" MODIFIED="1660909922544"/>
+<node TEXT="link na web" ID="ID_149494469" CREATED="1660909924527" MODIFIED="1660909931764"/>
+</node>
+</node>
+<node TEXT="hodnotenie stanic" ID="ID_764895060" CREATED="1660909977119" MODIFIED="1660910676890"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      iba pre prihlasenych
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="hodnotiaci" ID="ID_737708742" CREATED="1660910119155" MODIFIED="1660910126240">
+<node TEXT="zaujemca o macku" ID="ID_1747427268" CREATED="1660910127499" MODIFIED="1660910552248"/>
+<node TEXT="chovna stanica" ID="ID_796632625" CREATED="1660910141908" MODIFIED="1660910556175"/>
+<node TEXT="majitel macky z danej stanice" ID="ID_902597525" CREATED="1660910558350" MODIFIED="1660910575296"/>
+</node>
+</node>
+<node TEXT="formulare" ID="ID_1977388662" CREATED="1660912926045" MODIFIED="1660913284250">
+<node TEXT="ktora macka je pre mna vhodna?" ID="ID_1851136452" CREATED="1660912934569" MODIFIED="1660913124646"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      vahovany priemer
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="short" ID="ID_1759326124" CREATED="1660913313441" MODIFIED="1660913336852"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      purina
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="long" ID="ID_1457190782" CREATED="1660913317160" MODIFIED="1660913320266"/>
+</node>
+<node TEXT="rasa macky podla fotky" ID="ID_1201971918" CREATED="1660912961429" MODIFIED="1660913108190"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      CNN
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="prepojenie" ID="ID_1553186691" CREATED="1661161354894" MODIFIED="1661161358175">
+<node TEXT="google analytics" ID="ID_877115336" CREATED="1661161359374" MODIFIED="1661161363193"/>
+<node TEXT="facebook pixel" ID="ID_719865662" CREATED="1661161366654" MODIFIED="1661161372478"/>
+</node>
+</node>
+<node TEXT="uzivatelia" POSITION="right" ID="ID_609760265" CREATED="1660910398943" MODIFIED="1660910402617">
+<edge COLOR="#ff00ff"/>
+<node TEXT="chovatelska stanica" ID="ID_1222992260" CREATED="1660910409382" MODIFIED="1660910413985">
+<node TEXT="obligatorne" ID="ID_364354867" CREATED="1664368377178" MODIFIED="1664368385463">
+<node TEXT="meno a priezvisko" ID="ID_554588119" CREATED="1664368341234" MODIFIED="1664368347527"/>
+<node TEXT="adresa" ID="ID_960794585" CREATED="1664368349650" MODIFIED="1664368351951"/>
+<node TEXT="mail a telefon" ID="ID_81583952" CREATED="1664368356099" MODIFIED="1664368359134"/>
+<node TEXT="kde je registrovany" ID="ID_1618122990" CREATED="1664368361210" MODIFIED="1664368366087"/>
+<node TEXT="ktore plemena chova" ID="ID_364795751" CREATED="1664368368051" MODIFIED="1664368371889"/>
+<node TEXT="datum zalozenia" ID="ID_1961335310" CREATED="1664368407816" MODIFIED="1664368410965"/>
+<node TEXT="hodnotenie stanice" ID="ID_1948737608" CREATED="1664368452171" MODIFIED="1664368461822"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      hviezdicky
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="fakultativne" ID="ID_1035454228" CREATED="1664368391334" MODIFIED="1664368394658">
+<node TEXT="popis stanice" ID="ID_1219652602" CREATED="1664368415238" MODIFIED="1664368420842"/>
+<node TEXT="galeria" ID="ID_630159967" CREATED="1664368423026" MODIFIED="1664368433611"/>
+</node>
+</node>
+<node TEXT="administrator" ID="ID_298361384" CREATED="1660910419567" MODIFIED="1664968235859"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      mozu byt aj viaceri
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="recenzie" ID="ID_1614415226" CREATED="1660910437919" MODIFIED="1660910454760">
+<node TEXT="pridava" ID="ID_785280050" CREATED="1660910456095" MODIFIED="1660910458229"/>
+<node TEXT="schvaluje" ID="ID_1691313192" CREATED="1660910459704" MODIFIED="1660910462136"/>
+<node TEXT="edituje" ID="ID_626852709" CREATED="1660910463783" MODIFIED="1660910465649"/>
+<node TEXT="maze" ID="ID_113576968" CREATED="1660910467055" MODIFIED="1660910469264"/>
+</node>
+<node TEXT="chovatelske stanice" ID="ID_265708201" CREATED="1664968207661" MODIFIED="1664968212004">
+<node TEXT="pridava" ID="ID_754420752" CREATED="1660910456095" MODIFIED="1660910458229"/>
+<node TEXT="schvaluje" ID="ID_794413507" CREATED="1660910459704" MODIFIED="1660910462136"/>
+<node TEXT="edituje" ID="ID_169858853" CREATED="1660910463783" MODIFIED="1660910465649"/>
+<node TEXT="maze" ID="ID_809050196" CREATED="1660910467055" MODIFIED="1660910469264"/>
+</node>
+</node>
+<node TEXT="zaujemca o macku" ID="ID_1982419345" CREATED="1660910403775" MODIFIED="1660910407945">
+<node TEXT="recenzie" ID="ID_967478603" CREATED="1660910473786" MODIFIED="1664368512877"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ch0vatelskej stanice
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="pridava" ID="ID_1776589369" CREATED="1660910482529" MODIFIED="1660910484288"/>
+<node TEXT="moze editovat svoje hodnotenie" ID="ID_285696955" CREATED="1660910486183" MODIFIED="1660910493902"/>
+<node TEXT="moze zmazat svoje hodnotenie" ID="ID_399627678" CREATED="1660910495348" MODIFIED="1660910501792"/>
+</node>
+<node TEXT="oblubene stanice" ID="ID_61342607" CREATED="1660910504015" MODIFIED="1660910511312">
+<node TEXT="pridava do svojich" ID="ID_1230961336" CREATED="1660910512336" MODIFIED="1660910520744"/>
+<node TEXT="maze zo svojich" ID="ID_1405095874" CREATED="1660910522159" MODIFIED="1660910526832"/>
+</node>
+</node>
+</node>
+<node TEXT="prihlasovanie" POSITION="left" ID="ID_1463024660" CREATED="1660910694334" MODIFIED="1660910698022">
+<edge COLOR="#00ffff"/>
+<node TEXT="cez fb" ID="ID_1757065472" CREATED="1660910699199" MODIFIED="1660910705825"/>
+<node TEXT="cez google konto" ID="ID_181411080" CREATED="1660910707279" MODIFIED="1660910712043"/>
+<node TEXT="login/pass" ID="ID_1980515828" CREATED="1660910714094" MODIFIED="1660910719009"/>
+<node TEXT="verifikacia cez mail" ID="ID_1706596531" CREATED="1664368584944" MODIFIED="1664368594477"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      pri zalozeni uctu
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="struktura" POSITION="right" ID="ID_524519040" CREATED="1661161244378" MODIFIED="1661161246825">
+<edge COLOR="#007c00"/>
+<node TEXT="home" ID="ID_1045055742" CREATED="1661161248527" MODIFIED="1661161251447">
+<node TEXT="ciselnik" ID="ID_1776096493" CREATED="1661161293006" MODIFIED="1661161311400">
+<node TEXT="pocet stanic v systeme" ID="ID_235066616" CREATED="1661161313110" MODIFIED="1661161318295"/>
+<node TEXT="pocet navstevnikov" ID="ID_1379189318" CREATED="1661161319894" MODIFIED="1661161326648"/>
+<node TEXT="pocet hodnoteni" ID="ID_1152387214" CREATED="1661161328317" MODIFIED="1661161335272"/>
+</node>
+</node>
+<node TEXT="o projekte" ID="ID_166490707" CREATED="1661161252917" MODIFIED="1661161255802"/>
+<node TEXT="vyber si zviera" ID="ID_1355463958" CREATED="1661161257622" MODIFIED="1661161270682"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      formular a CNN
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="chovatelske stanice v sr" ID="ID_40498378" CREATED="1661161272590" MODIFIED="1661161278759">
+<node TEXT="zoznam + filter" ID="ID_689327159" CREATED="1664968285530" MODIFIED="1664968291680"/>
+<node TEXT="detail stanice" ID="ID_1404922349" CREATED="1664968310790" MODIFIED="1664968315131">
+<node TEXT="info" ID="ID_1812118777" CREATED="1664968318206" MODIFIED="1664968320260"/>
+<node TEXT="ranking" ID="ID_779538249" CREATED="1664968322904" MODIFIED="1664968325527"/>
+<node TEXT="komentare" ID="ID_1897762787" CREATED="1664968328020" MODIFIED="1664968332506"/>
+</node>
+</node>
+<node TEXT="kontakt" ID="ID_1010446956" CREATED="1664966859195" MODIFIED="1664966868781"/>
+</node>
+<node TEXT="db" POSITION="left" ID="ID_28995267" CREATED="1664970362675" MODIFIED="1664970798004">
+<edge COLOR="#7c007c"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      monika doda csv
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="chovne stanice" ID="ID_1320935015" CREATED="1664970806468" MODIFIED="1664970811486">
+<node TEXT="meno" ID="ID_236010039" CREATED="1664970366100" MODIFIED="1664970368199"/>
+<node TEXT="datum zalozenia" ID="ID_1615224431" CREATED="1664970370754" MODIFIED="1664970376801"/>
+<node TEXT="vlastnik" ID="ID_1708485033" CREATED="1664970378860" MODIFIED="1664970380905"/>
+<node TEXT="adresa" ID="ID_979039957" CREATED="1664970383703" MODIFIED="1664970385472"/>
+<node TEXT="plemeno" ID="ID_1303023232" CREATED="1664970388266" MODIFIED="1664970394177"/>
+<node TEXT="hodnotenie" ID="ID_1971475022" CREATED="1664970611551" MODIFIED="1664970616870"/>
+<node TEXT="pocet vrhov" ID="ID_1360630311" CREATED="1664970632813" MODIFIED="1664970643780"/>
+<node TEXT="mail" ID="ID_1444179194" CREATED="1664970670136" MODIFIED="1664970672445"/>
+<node TEXT="telefon" ID="ID_724526681" CREATED="1664970675227" MODIFIED="1664970678054"/>
+<node TEXT="web" ID="ID_1785963364" CREATED="1664970774495" MODIFIED="1664970778025"/>
+</node>
+<node TEXT="plemena" ID="ID_307687586" CREATED="1664970820106" MODIFIED="1664970824461"/>
+<node TEXT="uzivatelia" ID="ID_1792878429" CREATED="1664970916569" MODIFIED="1664970919956"/>
+<node TEXT="hodnotenia" ID="ID_1825807298" CREATED="1664970922683" MODIFIED="1664970925808"/>
+</node>
+</node>
+</map>
